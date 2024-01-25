eb27d114b19c631695a59d35b0bb6ff771e2abf0
ikedecrocs <mateuscaden@hotmail.com>
2024-01-25 01:08:45 -0300

    Commit inicial

---
+++ src/main/java/com/listas/apilistasreproducao/ApiListasReproducaoApplication.java
@@ -0,0 +1,13 @@
+package com.listas.apilistasreproducao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiListasReproducaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiListasReproducaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/listas/apilistasreproducao/ApiListasReproducaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.listas.apilistasreproducao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiListasReproducaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
